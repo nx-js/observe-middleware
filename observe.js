1d194b71d6d44d62f06279cbed0d542bc6ae70b5
Miklos Bertalan <miklos.bertalan@risingstack.com>
2017-01-02 15:48:05 +0100

    initial commit

---
+++ observe.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const observer = require('@nx-js/observer-util')
+
+function observe (node, state) {
+  node.$contextState = observer.observable(node.$contextState)
+  node.$state = observer.observable(node.$state)
+
+  node.$observe = $observe
+  node.$queue = $queue
+  node.$unobserve = observer.unobserve
+}
+observe.$name = 'observe'
+module.exports = observe
+
+function $observe (fn, ...args) {
+  args.unshift(fn, this)
+  const signal = observer.observe.apply(null, args)
+  this.$cleanup(observer.unobserve, signal)
+  return signal
+}
+
+function $queue (fn, ...args) {
+  args.unshift(fn, this)
+  return observer.queue.apply(null, args)
+}
